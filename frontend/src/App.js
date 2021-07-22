fea028568762782eb935a94050a7642804351b5c
multicampus <dvlprjw@gmail.com>
2021-07-22 17:17:52 +0900

    Update Routing Author : PJW Date : 21.07.22 17:17

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import Header from "./components/header/Header"
+import { BrowserRouter, Route } from "react-router-dom"
 import "./App.css"
+import MainPage from "./router/game/common/MainPage.js"
+import Login from "./router/user/common/login.js"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <BrowserRouter>
+        <Route path="/" exact={true} component={MainPage}></Route>
+        <Route path="/login" exact={true} component={Login}></Route>
+      </BrowserRouter>
     </div>
   );
 }
