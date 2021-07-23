298ded8247e8ae57551ce00a9a8825fc20913aa8
Jang Hyeon Woong <ds6tvi@gmail.com>
2021-07-23 17:43:09 +0900

    Update profile Author : JHW Date : 21.07.23 17:43

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-import Header from "./components/header/Header"
-import { BrowserRouter, Route } from "react-router-dom"
-import "./App.css"
-import MainPage from "./router/game/common/MainPage.js"
-import Login from "./router/user/common/login.js"
-import Register from "./router/user/common/register.js"
+import Header from './components/header/Header';
+import { BrowserRouter, Route } from 'react-router-dom';
+import './App.css';
+import MainPage from './router/game/common/MainPage.js';
+import Login from './router/user/common/Login.js';
+import Register from './router/user/common/Register.js';
+import Profile from './router/user/common/Profile';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" exact={true} component={MainPage}></Route>
         <Route path="/accounts/login" exact={true} component={Login}></Route>
         <Route path="/accounts/register" exact={true} component={Register}></Route>
+        <Route path="/profile" exact={true} component={Profile} />
       </BrowserRouter>
     </div>
   );
