9d3ed3ba0510bf56a7e46a5d34ac513f91a3414e
SOEUN2 <0_9_6@naver.com>
2021-07-27 16:18:52 +0900

    Update : backend\readUserAuth - backend\readUserAuth : 로그인 기능 구현 완료 Author : LSE Date : 21.07.27 16:18

--- backend/src/main/java/com/osds/bitz/controller/account/user/UserAuthController.java
+++ backend/src/main/java/com/osds/bitz/controller/account/user/UserAuthController.java
@@ -1,8 +1,10 @@
 package com.osds.bitz.controller.account.user;
 
+import com.osds.bitz.model.request.ReadUserAuthRequest;
 import com.osds.bitz.model.request.UpdatePasswordRequest;
 import com.osds.bitz.model.request.UserAuthRequest;
 import com.osds.bitz.model.response.UserAuthResponse;
+import com.osds.bitz.repository.account.user.UserAuthRepository;
 import com.osds.bitz.service.account.user.UserAuthService;
 
 import io.swagger.annotations.Api;
@@ -11,7 +13,6 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.sql.Update;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,16 +28,22 @@ public class UserAuthController {
     private UserAuthService userAuthService;
 
     @PostMapping("/createuser")
-    @ApiOperation(value = "가입하기", notes = "회원의 정보를 DB에 저장합니다.")
+    @ApiOperation(value = "회원가입", notes = "회원의 정보를 DB에 저장합니다.")
     public ResponseEntity<UserAuthResponse> createUser(@RequestBody @ApiParam(value = "회원 정보") UserAuthRequest userAuthRequest) throws Exception{
         UserAuthResponse response = new UserAuthResponse(userAuthService.createUser(userAuthRequest));
+        return new ResponseEntity<UserAuthResponse>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/readuser")
+    @ApiOperation(value = "로그인", notes = "회원의 정보를 통해 로그인 처리를 합니다.")
+    public ResponseEntity<UserAuthResponse> readUser(@RequestBody @ApiParam(value="회원 정보") ReadUserAuthRequest readUserAuthRequest) throws Exception {
+        UserAuthResponse response = new UserAuthResponse(userAuthService.readUser(readUserAuthRequest));
         return new ResponseEntity<UserAuthResponse>(response, HttpStatus.OK);
     }
 
     @PostMapping("/updatepassword")
     @ApiOperation(value="비밀번호 변경하기", notes="회원의 비밀번호를 DB에서 수정합니다.")
     public ResponseEntity<UserAuthResponse> updatePassword(@RequestBody @ApiParam(value="회원 정보") UpdatePasswordRequest updatePasswordRequest) throws Exception{
-        log.info("updatePassword : {}", updatePasswordRequest);
         UserAuthResponse response = new UserAuthResponse(userAuthService.updatePassword(updatePasswordRequest));
         return new ResponseEntity<UserAuthResponse>(response, HttpStatus.OK);
     }
