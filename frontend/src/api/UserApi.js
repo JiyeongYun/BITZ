559960ec727784b3af995a61887e372e095f0848
Jang Hyeon Woong <ds6tvi@gmail.com>
2021-07-28 14:33:25 +0900

    Update: api Author : JHW Date : 21.07.28 14:33

--- frontend/src/api/UserApi.js
+++ frontend/src/api/UserApi.js
@@ -12,7 +12,7 @@ const requestLogin = (data, callback, errorCallback) => {
 }; // requestLogin End
 
 // PJW - 회원가입 axios 요청
-const requsetJoin = (data, callback, errorCallback) => {
+const requestJoin = (data, callback, errorCallback) => {
   axios({
     method: 'post',
     url: 'http://127.0.0.1:8080/account/createuser',
@@ -23,7 +23,7 @@ const requsetJoin = (data, callback, errorCallback) => {
 }; // requestJoin End
 
 const UserApi = {
-  requsetJoin: (data, callback, errorCallback) => requsetJoin(data, callback, errorCallback),
+  requestJoin: (data, callback, errorCallback) => requestJoin(data, callback, errorCallback),
   requestLogin: (data, callback, errorCallback) => requestLogin(data, callback, errorCallback),
 };
 
