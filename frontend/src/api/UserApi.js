c33432666acb5aac862c1ce3390df80a79281e7a
Jang Hyeon Woong <ds6tvi@gmail.com>
2021-07-28 13:55:24 +0900

    Update: api Author : JHW Date : 21.07.28 13:55

--- frontend/src/api/UserApi.js
+++ frontend/src/api/UserApi.js
@@ -1,20 +1,29 @@
-import axios from "axios";
- 
-// PJW - 회원가입 axios 요청
-const requsetJoin = (data,callback,errorCallback) => {
+import axios from 'axios';
 
-    axios({
-        method: "post",
-        url: "http://127.0.0.1:8080/account/signup",
-        data: data
-    })
+// JHW - 로그인 axios 요청
+const requestLogin = (data, callback, errorCallback) => {
+  axios({
+    method: 'post',
+    url: 'http://127.0.0.1:8080/account/readuserauth',
+    data: data,
+  })
     .then(callback)
-    .catch(errorCallback)
- 
-} // requestJoin End
- 
-const UserApi = {
-    requsetJoin:(data,callback,errorCallback)=>requsetJoin(data,callback,errorCallback),
-}
+    .catch(errorCallback);
+}; // requestLogin End
 
-export default UserApi 
+// PJW - 회원가입 axios 요청
+const requsetJoin = (data, callback, errorCallback) => {
+  axios({
+    method: 'post',
+    url: 'http://127.0.0.1:8080/account/createuser',
+    data: data,
+  })
+    .then(callback)
+    .catch(errorCallback);
+}; // requestJoin End
+
+const UserApi = {
+  requsetJoin: (data, callback, errorCallback) => requsetJoin(data, callback, errorCallback),
+};
+
+export default UserApi;
