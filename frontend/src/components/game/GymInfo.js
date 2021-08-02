ca8acf0b42623ab0f809eaec3f018eae77e8f9fa
권오우 <kwonow1@naver.com>
2021-08-02 17:36:50 +0900

    Update : detail - GymInfo 수정 Author : KOW Date : 21.08.02 17:36

--- frontend/src/components/game/GymInfo.js
+++ frontend/src/components/game/GymInfo.js
@@ -9,60 +9,58 @@ const GymInfo = () => {
   const [scoreboard, setScoreboard] = useState(true);
   const [parking, setParking] = useState(true);
   const [basketball, setBasketball] = useState(true);
-  const [airconditioner, setAirconditioner] = useState(true);
+  const [airconditioner, setAirconditioner] = useState(false);
 
   useEffect(() => {
     if (!water) {
-      document.getElementsByName('water')[0].classList.add('no');
+      document.querySelector('#water').classList.add('no')
     }
     if (!shower) {
-      document.getElementsByName('shower')[0].classList.add('no');
+      document.querySelector('#shower').classList.add('no');
     }
     if (!scoreboard) {
-      document.getElementsByName('scoreboard')[0].classList.add('no');
+      document.querySelector('#scoreboard').classList.add('no');
     }
     if (!parking) {
-      document.getElementsByName('parking')[0].classList.add('no');
+      document.querySelector('#parking').classList.add('no');
     }
     if (!basketball) {
-      document.getElementsByName('basketball')[0].classList.add('no');
+      document.querySelector('#basketball').classList.add('no');
     }
     if (!airconditioner) {
-      document.getElementsByName('airconditioner')[0].classList.add('no');
+      document.querySelector('#airconditioner').classList.add('no');
     }
   });
 
   return (
     <div className="gymInfo">
-      <div className="introduce">
-        저희 코트장의 코트 규격은 <input type="text" placeholder="28m x 15m" /> 입니다.
-        <br />
-        편의시설은,
-        <br />
-      </div>
-      <div className="facilities__div">
-        <div className="facilities" name="water" id="water" />
-        <p>정수기</p>
-      </div>
-      <div className="facilities__div">
-        <div className="facilities" name="shower" id="shower" />
-        <p>샤워실</p>
-      </div>
-      <div className="facilities__div">
-        <div className="facilities" name="scoreboard" id="scoreboard" />
-        <p>점수판& 호루라기</p>
-      </div>
-      <div className="facilities__div">
-        <div className="facilities" name="parking" id="parking" />
-        <p>주차장</p>
-      </div>
-      <div className="facilities__div">
-        <div className="facilities" name="basketball" id="basketball" />
-        <p>농구장 대여</p>
-      </div>
-      <div className="facilities__div">
-        <div className="facilities" name="airconditioner" id="airconditioner" />
-        <p>에어컨</p>
+      <p>저희 경기장의 코트 규격은, <span>28m x 15m</span> 입니다.</p>
+      <p>편의시설은,</p>
+      <div className="facilities__container">
+        <div className="waterbox">
+          <div className="facility" name="water" id="water" />
+          <p>정수기</p>
+        </div>
+        <div className="showerbox">
+          <div className="facility" name="shower" id="shower" />
+          <p>샤워실</p>
+        </div>
+        <div className="scoreboardbox">
+          <div className="facility" name="scoreboard" id="scoreboard" />
+          <p>점수판 & 휘슬</p>
+        </div>
+        <div className="parkingbox">
+          <div className="facility" name="parking" id="parking" />
+          <p>주차장</p>
+        </div>
+        <div className="basketballbox">
+          <div className="facility" name="basketball" id="basketball" />
+          <p>농구공</p>
+        </div>
+        <div className="airconditionerbox">
+          <div className="facility" name="airconditioner" id="airconditioner" />
+          <p>에어컨 & 난방</p>
+        </div>
       </div>
     </div>
   );
