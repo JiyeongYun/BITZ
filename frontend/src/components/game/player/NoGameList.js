17e1068c52872ff8a75fc7089c0578218aabfc48
권오우 <kwonow1@naver.com>
2021-08-04 15:05:11 +0900

    Update : main.css - Main Page CSS 통일 Author : KOW Date : 21.08.04 15:05

--- frontend/src/components/game/player/NoGameList.js
+++ frontend/src/components/game/player/NoGameList.js
@@ -5,8 +5,10 @@ function NoGameList () {
 
     return (
         <div className="nogame">
-            <h2>예약 리스트가 없습니다</h2>
-            <Link to="/">예약하러 갈래요!</Link>
+            <div className="content">
+                <h2>예약 리스트가 없습니다</h2>
+                <Link to="/">예약하러 갈래요!</Link>
+            </div>
         </div>
     )
 }
