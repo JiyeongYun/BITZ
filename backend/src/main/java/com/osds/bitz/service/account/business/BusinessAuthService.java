5a861fa157978f56cd8d60b6bd21afd37ea39294
jiyeongYun <yoonjy1106@naver.com>
2021-08-04 11:34:18 +0900

    Update : backend\encodingPassword - backend\encodingPassword : user, business 비밀번호 암호화 작업 Author : YJY Date : 21.08.04 11:34

--- backend/src/main/java/com/osds/bitz/service/account/business/BusinessAuthService.java
+++ backend/src/main/java/com/osds/bitz/service/account/business/BusinessAuthService.java
@@ -2,20 +2,16 @@ package com.osds.bitz.service.account.business;
 
 import com.osds.bitz.model.entity.account.business.BusinessAuth;
 import com.osds.bitz.model.entity.account.business.BusinessProfile;
-import com.osds.bitz.model.entity.account.user.UserAuth;
-import com.osds.bitz.model.entity.account.user.UserProfile;
 import com.osds.bitz.model.entity.log.LoginLog;
 import com.osds.bitz.model.network.request.BusinessAuthRequest;
 import com.osds.bitz.model.network.request.ReadAuthRequest;
 import com.osds.bitz.model.network.request.UpdatePasswordRequest;
 import com.osds.bitz.repository.account.business.BusinessAuthRepository;
 import com.osds.bitz.repository.account.business.BusinessProfileRepository;
-import com.osds.bitz.repository.log.LoginLogRepository;
 import com.osds.bitz.service.account.BaseAuthService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
@@ -34,40 +30,36 @@ public class BusinessAuthService extends BaseAuthService {
     @Autowired
     private BusinessProfileRepository businessProfileRepository;
 
-    @Autowired
-    private LoginLogRepository loginLogRepository;
-
-    @Autowired
-    private JavaMailSender mailSender;
-
-    // 회원가입
+    /**
+     * 회원가입
+     */
     public BusinessAuth createBusiness(BusinessAuthRequest businessAuthRequest) throws IOException {
 
         // 이메일 중복체크
-        if(this.businessAuthRepository.getBusinessAuthByEmail(businessAuthRequest.getEmail()) != null)
+        if (this.businessAuthRepository.getBusinessAuthByEmail(businessAuthRequest.getEmail()) != null)
             return null;
 
         // businessId로 설정할 랜덤 값 생성
         String businessAuthId = generateRandomNumber(false);
+
         // businessId 중복 체크
         BusinessAuth duplicataionBusinessAuth = this.businessAuthRepository.getById(businessAuthId);
-        while(businessAuthId.equals(duplicataionBusinessAuth.getId())){
+        while (businessAuthId.equals(duplicataionBusinessAuth.getId())) {
             businessAuthId = generateRandomNumber(false);
         }
 
-            // 파일 로컬에 저장
-            File targetFile = new File("src/main/resources/static/imgs/" + businessAuthRequest.getBusinessRegistration().getOriginalFilename());
-            try {
-                InputStream fileStream = businessAuthRequest.getBusinessRegistration().getInputStream();
-                FileUtils.copyInputStreamToFile(fileStream, targetFile);
-            } catch (IOException e) {
+        // 파일 로컬에 저장
+        File targetFile = new File("src/main/resources/static/imgs/" + businessAuthRequest.getBusinessRegistration().getOriginalFilename());
+        try {
+            InputStream fileStream = businessAuthRequest.getBusinessRegistration().getInputStream();
+            FileUtils.copyInputStreamToFile(fileStream, targetFile);
+        } catch (IOException e) {
         }
-
 
         BusinessAuth businessAuth = BusinessAuth.builder()
                 .id(businessAuthId)
                 .email(businessAuthRequest.getEmail())
-                .password(businessAuthRequest.getPassword())
+                .password(encodingPassword(businessAuthRequest.getPassword()))
                 .birth(businessAuthRequest.getBirth())
                 .build();
 
@@ -82,22 +74,33 @@ public class BusinessAuthService extends BaseAuthService {
 
         BusinessAuth newBusinessAuth = this.businessAuthRepository.save(businessAuth);
         this.businessProfileRepository.save(businessProfile);
+
         return newBusinessAuth;
     }
 
-    // 로그인
+    /**
+     * 로그인
+     */
     public BusinessAuth readBusiness(ReadAuthRequest readAuthRequest) {
-        // 이메일과 비밀번호로 객체 찾아오기
-        return this.businessAuthRepository.findBusinessAuthByEmailAndPassword(readAuthRequest.getEmail(), readAuthRequest.getPassword());
+        // 이메일로 객체 찾아오기
+        BusinessAuth businessAuth = this.businessAuthRepository.getBusinessAuthByEmail(readAuthRequest.getEmail());
+
+        if (businessAuth == null) return null;
+
+        if (!passwordEncoder.matches(readAuthRequest.getPassword(), businessAuth.getPassword())) return null;
+
+        return businessAuth;
     }
 
-    // 첫 로그인인지 확인하기
-    public BusinessAuth readFirstBusinessAuthRequest(ReadAuthRequest readAuthRequest){
+    /**
+     * 최초 로그인 확인
+     */
+    public BusinessAuth readFirstBusinessAuthRequest(ReadAuthRequest readAuthRequest) {
 
         // 이메일로 로그인 로그 객체 찾아오기
         LoginLog loginLog = this.loginLogRepository.getLoginLogByUserEmailAndIsGeneral(readAuthRequest.getEmail(), false);
 
-        if(loginLog == null){               // 최초 로그인시
+        if (loginLog == null) {               // 최초 로그인시
             loginLog = LoginLog.builder()
                     .userEmail(readAuthRequest.getEmail())
                     .isGeneral(false)
@@ -108,27 +111,35 @@ public class BusinessAuthService extends BaseAuthService {
         return null;
     }
 
-    // 비밀번호 변경하기
+    /**
+     * 비밀번호 변경
+     */
     public BusinessAuth updatePassword(UpdatePasswordRequest updatePasswordRequest) {
-        // 이메일로 해당 객체 찾아오기
-        BusinessAuth newBusinessAuth = new BusinessAuth();
-        newBusinessAuth = this.businessAuthRepository.getBusinessAuthByEmail(updatePasswordRequest.getEmail());
+
+        // 이메일로 객체 찾아오기
+        BusinessAuth businessAuth = this.businessAuthRepository.getBusinessAuthByEmail(updatePasswordRequest.getEmail());
+
+        // 전달된 비밀번호가 기존 DB의 비밀번호와 일치하는지 체크
+        if (!passwordEncoder.matches(updatePasswordRequest.getPassword(), businessAuth.getPassword())) return null;
 
         // 변경할 비밀번호 설정하기
-        newBusinessAuth.setPassword(updatePasswordRequest.getNewPassword());
-        return this.businessAuthRepository.save(newBusinessAuth);
+        businessAuth.setPassword(encodingPassword(updatePasswordRequest.getNewPassword()));
+        return this.businessAuthRepository.save(businessAuth);
     }
 
-    // 비밀번호 찾기
+    /**
+     * 비밀번호 찾기
+     */
     public BusinessAuth readPassword(BusinessAuthRequest businessAuthRequest) {
-        // 이메일로 해당 객체 찾아오기
+
+        // 이메일로 객체 찾아오기
         BusinessAuth newBusinessAuth = this.businessAuthRepository.getBusinessAuthByEmail(businessAuthRequest.getEmail());
 
         // 임시 비밀번호 생성 및 메일 전송
-        String code = "";
+        String tempPassword = "";
         try {
-            code = generateRandomNumber();
-            String msg = "<p><b> " + newBusinessAuth.getEmail() + " </b>님의 임시 비밀번호입니다.</p> <p style=color:red;> <h1>" + code + "</h1> </p>\n \n 로 새롭게 로그인 후 비밀번호를 변경해주세요!";
+            tempPassword = generateRandomNumber();
+            String msg = "<p><b> " + newBusinessAuth.getEmail() + " </b>님의 임시 비밀번호입니다.</p> <p style=color:red;> <h1>" + tempPassword + "</h1> </p>\n \n 로 새롭게 로그인 후 비밀번호를 변경해주세요!";
             MimeMessage message = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
             helper.setFrom("OSDS"); // 보내는 사람
@@ -142,12 +153,14 @@ public class BusinessAuthService extends BaseAuthService {
         }
 
         // 임시 비밀번호로 비밀번호 변경하기
-        newBusinessAuth.setPassword(code);
+        newBusinessAuth.setPassword(encodingPassword(tempPassword));
         return this.businessAuthRepository.save(newBusinessAuth);
     }
 
-    // 회원탈퇴
-    public void deleteBusinessAuth(ReadAuthRequest readAuthRequest){
+    /**
+     * 회원탈퇴
+     */
+    public void deleteBusinessAuth(ReadAuthRequest readAuthRequest) {
         BusinessAuth businessAuth = this.businessAuthRepository.getBusinessAuthByEmail(readAuthRequest.getEmail());
         BusinessProfile businessProfile = this.businessProfileRepository.getBusinessProfileByBusinessAuth(businessAuth);
 
