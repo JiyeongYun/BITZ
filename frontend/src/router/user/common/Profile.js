b9d5cf4b6d64cb05ed79608ce3c9b223c927d057
권오우 <kwonow1@naver.com>
2021-08-08 20:09:13 +0900

    Update: myprofile - 프로필 페이지 서버와 연결 및 정보 수정 가능 Author : KOW Date : 21.08.08 20:09

--- frontend/src/router/user/common/Profile.js
+++ frontend/src/router/user/common/Profile.js
@@ -1,15 +1,20 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import MyInfo from '../../../components/user/player/profile/MyInfo';
 import Score from '../../../components/user/player/profile/Score';
 import MyGym from '../../../components/user/business/profile/MyGym';
 import './Profile.css';
 import { Link } from 'react-router-dom';
 import { store } from 'store/store.js'; // store import (store)
+import UserApi from 'api/UserApi';
+
 const Profile = ({ history }) => {
   // 전역 상태 관리 (store)
   const globalState = useContext(store);
   const { value, dispatch } = globalState;
   const { userKind, userObj } = value; // 플레이어, 비즈니스 구분용 전역 State
+
+  // 유저 정보가 담긴 State
+  const [userData, setUserData] = useState({email: "", birth: ""})
 
   const onLogout = () => {
     localStorage.removeItem("currentUser")
@@ -17,19 +22,35 @@ const Profile = ({ history }) => {
     dispatch({ type: "LOGIN", value: "" })
     history.push('/')
   };
+  
+
+  // 유저 정보를 DB에서 가져오기
+  useEffect(() => {
+    const params = {email:value.isLogin}
+    UserApi.myprofile(
+      params,
+      res => {
+        setUserData(res.data)
+      },
+      err => {
+        console.log(err)
+      }
+    )
+  }, [value.isLogin])
 
   return (
     <div className="profile__div">
       <div className="user__profile">
         <img src="/images/KOW.png" alt="profile" />
         <p id="nickname">덩크하는 물소</p>
-        <p id="email">{globalState.value.isLogin}</p>
+        <p id="email">{userData.email}</p>
+        <p id="birth">{userData.birth.slice(0,4)}.{userData.birth.slice(4,6)}.{userData.birth.slice(6)}</p>
       </div>
       {/* 유저가 플레이어일 경우 */}
       {userKind==='player' && (
         <div className="left_right_profile">
-          <Score userObj={userObj} />
-          <MyInfo userObj={userObj} />
+          <Score userObj={userObj} userData={userData} />
+          <MyInfo userObj={userObj} userData={userData} setUserData={setUserData} />
         </div>
       )}
       {/* 유저가 비즈니스일 경우 */}
