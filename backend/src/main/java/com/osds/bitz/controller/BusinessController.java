8d19ac110c4b1bb31f26a98a0e8c1b498f70abfa
jiyeongYun <yoonjy1106@naver.com>
2021-08-11 14:53:59 +0900

    Update : backend\myPickupGameList - backend\myPickupGameList : 비즈니스 계정 메인페이지에서 나의 픽업게임리스트 가져오기 Author : YJY Date : 21.08.11 14:53

--- backend/src/main/java/com/osds/bitz/controller/BusinessController.java
+++ backend/src/main/java/com/osds/bitz/controller/BusinessController.java
@@ -1,14 +1,13 @@
 package com.osds.bitz.controller;
 
 import com.osds.bitz.model.entity.account.business.BusinessAuth;
-import com.osds.bitz.model.entity.account.business.BusinessProfile;
+import com.osds.bitz.model.entity.game.Game;
 import com.osds.bitz.model.network.request.account.BusinessAuthRequest;
 import com.osds.bitz.model.network.request.account.BusinessRequest;
 import com.osds.bitz.model.network.request.account.ReadAuthRequest;
 import com.osds.bitz.model.network.request.account.UpdatePasswordRequest;
 import com.osds.bitz.model.network.response.account.BusinessAuthResponse;
 import com.osds.bitz.model.network.response.account.BusinessResponse;
-import com.osds.bitz.model.network.response.account.UserResponse;
 import com.osds.bitz.service.BusinessService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -18,6 +17,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.Date;
+import java.util.ArrayList;
 
 @CrossOrigin("*")
 @AllArgsConstructor
@@ -56,16 +58,16 @@ public class BusinessController {
     @PostMapping("/loginlog")
     @ApiOperation(value = "첫 로그인인지 확인", notes = "회원이 처음으로 로그인했는지 DB에서 확인합니다.")
     public ResponseEntity<BusinessAuthResponse> readLoginLog(@RequestBody @ApiParam(value = "회원 정보") ReadAuthRequest readAuthRequest) throws Exception {
-        if(!businessService.readLoginLog(readAuthRequest.getEmail()))  // 최초로그인이 아닌 경우
+        if (!businessService.readLoginLog(readAuthRequest.getEmail()))  // 최초로그인이 아닌 경우
             return new ResponseEntity(HttpStatus.NO_CONTENT);
         return new ResponseEntity(HttpStatus.OK);                       // 최초로그인인 경우
     }
 
     @GetMapping("/businessprofile")
     @ApiOperation(value = "마이페이지 정보 조회", notes = "회원의 마이페이지 정보를 조회합니다.")
-    public ResponseEntity<BusinessResponse> readProfile(@ApiParam(value = "회원 정보") @RequestParam(value="email") String email) throws Exception {
+    public ResponseEntity<BusinessResponse> readProfile(@ApiParam(value = "회원 정보") @RequestParam(value = "email") String email) throws Exception {
         BusinessResponse businessResponse = businessService.readProfile(email);
-        if(businessResponse == null)
+        if (businessResponse == null)
             return new ResponseEntity(HttpStatus.NOT_FOUND);
         return ResponseEntity.status(HttpStatus.OK).body(businessResponse);
     }
@@ -77,12 +79,21 @@ public class BusinessController {
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    @GetMapping("/game/list")
+    @ApiOperation(value = "메인 페이지 조회", notes = "자신이 올린 게임 리스트를 조회합니다.")
+    public ResponseEntity<ArrayList<Game>> readGameList(@ApiParam(value = "회원 정보") @RequestParam(value = "email") String email, @RequestParam(value = "date") Date date) throws Exception {
+        ArrayList<Game> response = businessService.readGameList(email, date);
+        if (response == null)
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
     @PutMapping("/businessauth/password/change")
     @ApiOperation(value = "비밀번호 변경", notes = "회원의 비밀번호를 DB에서 수정합니다.")
     public ResponseEntity<BusinessAuthResponse> updatePassword(@RequestBody @ApiParam(value = "회원 정보") UpdatePasswordRequest updatePasswordRequest) throws Exception {
         BusinessAuth businessAuth = businessService.updatePassword(updatePasswordRequest);
-        if(businessAuth == null)
-        return new ResponseEntity(HttpStatus.NOT_FOUND);
+        if (businessAuth == null)
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
         return new ResponseEntity(HttpStatus.OK);
     }
 
@@ -90,7 +101,7 @@ public class BusinessController {
     @ApiOperation(value = "비밀번호 찾기", notes = "회원의 이메일에 비밀번호를 찾아서 임시 비밀번호를 이메일로 전송합니다.")
     public ResponseEntity<BusinessAuthResponse> resetPassword(@RequestBody @ApiParam(value = " 회원 정보") BusinessAuthRequest businessAuthRequest) throws Exception {
         BusinessAuth businessAuth = businessService.resetPassword(businessAuthRequest);
-        if(businessAuth == null)
+        if (businessAuth == null)
             return new ResponseEntity(HttpStatus.NOT_FOUND);
         return new ResponseEntity(HttpStatus.OK);
     }
