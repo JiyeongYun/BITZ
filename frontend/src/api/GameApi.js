5a2e2d7dacaa212b8cb558bd1c42669f15330d6f
권오우 <kwonow1@naver.com>
2021-08-12 18:13:52 +0900

    Update : gameapi - 머지 충돌로 인한 수정 Author : KOW Date : 21.08.12 18:13

--- frontend/src/api/GameApi.js
+++ frontend/src/api/GameApi.js
@@ -83,6 +83,41 @@ const ConfirmReservation = (data,callback,errorCallback) => {
     .catch(errorCallback)
 }
 
+// PJW - Game 리뷰 여부 확인
+const ReviewCheck = (data,callback,errorCallback) => {
+  axios({
+    method: "GET",
+    url: BASE_URL + "/game/review",
+    params: data
+  })
+  .then(callback)
+  .catch(errorCallback)
+}
+
+// PJW - Game 리뷰 제출
+const ApplyReview = (data,callback,errorCallback) => {
+  axios({
+    method: "POST",
+    url: BASE_URL + "/game/review",
+    data: data
+  })
+  .then(callback)
+  .catch(errorCallback)
+}
+
+// PJW - 쿼터별 경기 기록
+const RecordGame = (data,callback,errorCallback) => {
+  axios({
+    method: "POST",
+    url: BASE_URL + "/game/review",
+    data: data
+  })
+  .then(callback)
+  .catch(errorCallback)
+}
+
+
+
 // KOW - 내가 만든 게임 리스트
 const myGameList = (data, callback, errorCallback) => {
   axios({
@@ -102,6 +137,9 @@ const GameApi = {
   requsetPaymentCheck:(data,callback,errorCallback)=>requsetPaymentCheck(data,callback,errorCallback),
   cancelReservation:(data,callback,errorCallback)=>cancelReservation(data,callback,errorCallback),
   ConfirmReservation:(data,callback,errorCallback)=>ConfirmReservation(data,callback,errorCallback),
+  ApplyReview:(data,callback,errorCallback)=>ApplyReview(data,callback,errorCallback),
+  ReviewCheck:(data,callback,errorCallback)=>ReviewCheck(data,callback,errorCallback),
+  RecordGame:(data,callback,errorCallback)=>RecordGame(data,callback,errorCallback),
   myGameList:(data,callback,errorCallback)=>myGameList(data,callback,errorCallback),
 }
 
