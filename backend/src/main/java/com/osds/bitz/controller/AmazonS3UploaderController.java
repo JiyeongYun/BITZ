30e9b27e4d7ae58ce1957c948665cf061c24d185
SOEUN2 <0_9_6@naver.com>
2021-08-12 14:28:23 +0900

    Update : amazons3upload amazons3upload : 프론트 응답 URL 전송 Author : LSE Date : 21.08.12 14:28

--- backend/src/main/java/com/osds/bitz/controller/AmazonS3UploaderController.java
+++ backend/src/main/java/com/osds/bitz/controller/AmazonS3UploaderController.java
@@ -2,8 +2,10 @@ package com.osds.bitz.controller;
 
 import com.osds.bitz.service.AmazonS3UploaderService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,29 +18,40 @@ import java.io.IOException;
 @RequestMapping("amazons3/upload")
 @Slf4j
 public class AmazonS3UploaderController {
-    private final AmazonS3UploaderService amazonS3UploaderService3Uploader;
+    private final AmazonS3UploaderService amazonS3UploaderService;
+
+    @Value("${cloud.aws.s3.bucket}")
+    public String bucket;  // S3 버킷 이름
 
     @PostMapping("/userprofile")
+    @ApiOperation(value = "사용자 프로필 이미지 저장", notes = "사용자 프로필 이미지를 AMAZON S3에 저장합니다.")
     public String uploadUserProfile(@RequestParam("images") MultipartFile multipartFile) throws IOException {
-        amazonS3UploaderService3Uploader.upload(multipartFile, "userprofile");
-        return "사용자 프로필 이미지 업로드 완료";
+        String responseURL = amazonS3UploaderService.upload(multipartFile, "userprofile");
+        log.info("{}", responseURL);
+        return responseURL;
     }
 
     @PostMapping("/businessauth")
+    @ApiOperation(value = "사업자등록증 이미지 저장", notes = "사용자등록증 이미지를 AMAZON S3에 저장합니다.")
     public String uploadBusinessAuth(@RequestParam("images") MultipartFile multipartFile) throws IOException {
-        amazonS3UploaderService3Uploader.upload(multipartFile, "businessauth");
-        return "사업자등록증 이미지 업로드 완료";
+        String responseURL = amazonS3UploaderService.upload(multipartFile, "businessauth");
+        log.info("{}", responseURL);
+        return responseURL;
     }
 
     @PostMapping("/businessprofile")
+    @ApiOperation(value = "사업자 프로필 이미지 저장", notes = "사업자 프로필 이미지를 AMAZON S3에 저장합니다.")
     public String uploadBusinessProfile(@RequestParam("images") MultipartFile multipartFile) throws IOException {
-        amazonS3UploaderService3Uploader.upload(multipartFile, "businessprofile");
-        return "사업자 프로필 이미지 업로드 완료";
+        String responseURL = amazonS3UploaderService.upload(multipartFile, "businessprofile");
+        log.info("{}", responseURL);
+        return responseURL;
     }
 
     @PostMapping("/gym")
+    @ApiOperation(value = "체육관 이미지 저장", notes = "체육관 이미지를 AMAZON S3에 저장합니다.")
     public String uploadGym(@RequestParam("images") MultipartFile multipartFile) throws IOException {
-        amazonS3UploaderService3Uploader.upload(multipartFile, "gym");
-        return "체육관 이미지 업로드 완료";
+        String responseURL = amazonS3UploaderService.upload(multipartFile, "gym");
+        log.info("{}", responseURL);
+        return responseURL;
     }
 }
