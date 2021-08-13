c238049cb8786526c60b6ded3a40843e6f93b477
SOEUN2 <0_9_6@naver.com>
2021-08-13 15:12:02 +0900

    Update : amazons3 image crud - amazons3 image crud : amazons3에 이미지 CRUD기능 완료 Author : LSE Date : 21.08.13 15:11

--- backend/src/main/java/com/osds/bitz/repository/img/BusinessProfileImageRepository.java
+++ backend/src/main/java/com/osds/bitz/repository/img/BusinessProfileImageRepository.java
@@ -3,7 +3,11 @@ package com.osds.bitz.repository.img;
 import com.osds.bitz.model.entity.account.business.BusinessAuth;
 import com.osds.bitz.model.entity.img.BusinessProfileImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface BusinessProfileImageRepository  extends JpaRepository<BusinessProfileImage,Long> {
-        BusinessProfileImage getBusinessProfileImageByBusinessAuth(BusinessAuth businessAuth);
+public interface BusinessProfileImageRepository extends JpaRepository<BusinessProfileImage, Long> {
+    BusinessProfileImage getBusinessProfileImageByBusinessAuth(BusinessAuth businessAuth);
+
+    @Transactional
+    void deleteAllByBusinessAuth(BusinessAuth businessAuthth);
 }
