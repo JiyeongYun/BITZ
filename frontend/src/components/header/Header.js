eff6a6ec5e10606bfb462543fc3d913e494c8973
권오우 <kwonow1@naver.com>
2021-08-15 22:47:58 +0900

    Update : css 헤더 css 변경 완료 Author : KOW Date  : 21.08.15 22:47

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -2,11 +2,15 @@ import React, { useCallback, useContext, useEffect, useState } from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
 import { store } from 'store/store.js'; // store import (store)
+import ImgApi from 'api/ImgApi';
 
 function Header(props) {
   // 전역 상태 관리 (store)
   const globalState = useContext(store);
   const [isScrollTop, setIsScrollTop] = useState(true)
+
+  // 프로필 이미지 State
+  const [imgUrl, setImgUrl] = useState(null)
   
   // const [offcanvas, setOffcanvas] = useState(false)
   // const toggleCanvas = () => {
@@ -32,6 +36,33 @@ function Header(props) {
     };
   }, [isScrollTop, handleScroll]);
 
+  useEffect(() => {
+    const params = {email : globalState.value.isLogin}
+    if (globalState.value.userKind === "player" ) {
+      ImgApi.getUserImg(
+        params,
+        res => {
+          const url = window.URL.createObjectURL(new Blob([res.data]))
+          setImgUrl(url)
+        },
+        err => {
+          console.log(err)
+        }
+      )
+    } else if (globalState.value.userKind === "business" ) {
+      ImgApi.getBusImg(
+        params,
+        res => {
+          const url = window.URL.createObjectURL(new Blob([res.data]))
+          setImgUrl(url)
+        },
+        err => {
+          console.log(err)
+        }
+      )
+    }
+  }, [globalState.value.isLogin, globalState.value.userKind])
+
   return (
     <div className={isScrollTop ? "header__container" : "header__container header__shadow"}>
       <div className="header">
@@ -41,18 +72,20 @@ function Header(props) {
         <div className="header__icons">
           {globalState.value.isLogin ? (
             <Link to={`/accounts/profile/${globalState.value.isLogin}`}>
-              <img src={process.env.PUBLIC_URL + `/images/profile.png`} width="20px" alt="my_profile" />
+              <img src={imgUrl} alt="my_profile" />
             </Link>
           ) : (
             <Link to="/accounts/login">
               <p className="icon">로그인</p>
             </Link>
           )}
-          <div className="icon menu__icon" onClick={props.toggleCanvas}>
+
+          {/* 메뉴바 로고 삭제 */}
+          {/* <div className="icon menu__icon" onClick={props.toggleCanvas}>
             <div className="circle__icon"></div>
             <div className="circle__icon"></div>
             <div className="circle__icon"></div>
-          </div>
+          </div> */}
         </div>
         {/* <div className={offcanvas ? "grey__canvas grey__canvas__show": "grey__canvas"} onClick={toggleCanvas}></div>
         <div className={offcanvas ? "offcanvas__show offcanvas": "offcanvas"}>
