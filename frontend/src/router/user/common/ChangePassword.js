83f2900337d738434ebb2ee10af931287de62af1
Jang Hyeon Woong <ds6tvi@gmail.com>
2021-08-17 11:03:28 +0900

    Update: frontend\findpassword - frontend\findpassword : 비밀번호 찾기 css 변경 Author : JHW Date : 21.08.17 11:03

--- frontend/src/router/user/common/ChangePassword.js
+++ frontend/src/router/user/common/ChangePassword.js
@@ -1,7 +1,7 @@
-import React, { useContext, useEffect, useState } from "react";
-import ChangePasswordValidation from "components/user/common/ChangePasswordValidation.js" // 유효성 검사 함수
-import "./ChangePassword.css" // Register.css의 내용을 많이 차용함 (Form이 비슷하기 때문)
-import UserApi from "api/UserApi";
+import React, { useContext, useEffect, useState } from 'react';
+import ChangePasswordValidation from 'components/user/common/ChangePasswordValidation.js'; // 유효성 검사 함수
+import './ChangePassword.css'; // Register.css의 내용을 많이 차용함 (Form이 비슷하기 때문)
+import UserApi from 'api/UserApi';
 import { store } from 'store/store.js'; // store import (store)
 
 function ChangePassword({ history }) {
@@ -12,68 +12,66 @@ function ChangePassword({ history }) {
   // State ***************************************************************
   // 입력 데이터
   const [values, setValues] = useState({
-    password: "",
-    newPassword: "",
-    newPasswordConfirm: "",
-  })
+    password: '',
+    newPassword: '',
+    newPasswordConfirm: '',
+  });
   // 유효성 검사 결과
   const [errors, setErrors] = useState({
     password: true,
     newPassword: true,
     newPasswordConfirm: true,
-  })
+  });
   // 최초 입력 확인
   const [isFirst, setIsFirst] = useState({
     password: true,
     newPassword: true,
     newPasswordConfirm: true,
-  })
+  });
   // 값을 모두 입력했는지 검증
-  const [isValidated, setIsValidated] = useState(false)
-
+  const [isValidated, setIsValidated] = useState(false);
 
   // useEffect ***************************************************************
   // PJW - 데이터 유효성 검증
-  useEffect(()=>{
-    setErrors({...ChangePasswordValidation(values, isFirst)})
-  }, [values, isFirst])
+  useEffect(() => {
+    setErrors({ ...ChangePasswordValidation(values, isFirst) });
+  }, [values, isFirst]);
   // 데이터 유효성 검증 End
 
   // 전체 데이터 유효성 검사
-  useEffect(()=>{
-    let check = true
-    Object.values(errors).forEach(value=>{
-      check = check && !value
-    })
-    setIsValidated(check)
-  }, [errors])
+  useEffect(() => {
+    let check = true;
+    Object.values(errors).forEach((value) => {
+      check = check && !value;
+    });
+    setIsValidated(check);
+  }, [errors]);
   // 전체 데이터 유효성 검사 End
-
 
   // methods ***************************************************************
   // PJW - 데이터 입력 시 값 업데이트
   const updateValue = (event) => {
     let { name, value } = event.target;
-    if (name==="password") {
-      setValues({ ...values, [name]: value, passwordConfirm: ""});
-      setIsFirst({...isFirst, passwordConfirm: true})
+    if (name === 'password') {
+      setValues({ ...values, [name]: value, passwordConfirm: '' });
+      setIsFirst({ ...isFirst, passwordConfirm: true });
     } else {
-      setValues({ ...values, [name]: value});
+      setValues({ ...values, [name]: value });
     }
-  } // updateValue End
+  }; // updateValue End
 
   // PJW - 에러 메시지 노출을 위해 최초 입력인지 확인
   const updateIsFirst = (event) => {
-    setIsFirst({...isFirst, [event.target.name]: false})
-  } // updateIsFirst End
+    setIsFirst({ ...isFirst, [event.target.name]: false });
+  }; // updateIsFirst End
 
   // PJW - Focus out 시 유효성 검사
   const lateValidateValue = (event) => {
-    const name = event.target.name
-    if (name==="newPasswordConfirm") {
-      setIsFirst({ ...isFirst, [name]: true});
+    const name = event.target.name;
+    if (name === 'newPasswordConfirm') {
+      setIsFirst({ ...isFirst, [name]: true });
     }
-  } // lateValidateValue End
+  }; // lateValidateValue End
 
   // PJW - 비밀번호 변경
   const onChangePassword = () => {
@@ -81,35 +79,34 @@ function ChangePassword({ history }) {
       email: isLogin,
       newPassword: values.newPassword,
       password: values.password,
-    }
+    };
 
     if (userKind === 'player') {
       UserApi.changePassword(
         data,
         () => {
-          alert('비밀번호가 변경되었습니다!')
-          history.push(`/accounts/profile/${isLogin}`)
+          alert('비밀번호가 변경되었습니다!');
+          history.push(`/accounts/profile/${isLogin}`);
         },
         () => {
           alert('비밀번호를 확인해주세요.');
         }
-      )
+      );
     } else if (userKind === 'business') {
       UserApi.changeBusPassword(
         data,
         () => {
-          alert('비밀번호가 변경되었습니다!')
-          history.push(`/accounts/profile/${isLogin}`)
+          alert('비밀번호가 변경되었습니다!');
+          history.push(`/accounts/profile/${isLogin}`);
         },
         () => {
           alert('비밀번호를 확인해주세요.');
         }
-      )
+      );
     }
+  }; // onChangePassword End
 
-  } // onChangePassword End
-
-  return(
+  return (
     <div className="changePassword">
       <div>
         <img className="changePassword__logo" src="/images/logo.png" alt="logo" />
@@ -119,34 +116,60 @@ function ChangePassword({ history }) {
         <div className="changePassword__password changePasswordForm__component">
           <label>현재 비밀번호 입력</label>
           <br />
-          <input className="inputBox" type="password" name="password" value={values.password} onChange={updateValue} onBlur={updateIsFirst}></input>
+          <input
+            className="inputBox"
+            type="password"
+            name="password"
+            value={values.password}
+            onChange={updateValue}
+            onBlur={updateIsFirst}
+          ></input>
           <div className="errorMessage">{errors.password}</div>
         </div>
         {/* 신 비밀번호 */}
         <div className="changePassword__password changePasswordForm__component">
           <label>새 비밀번호 입력</label>
           <br />
-          <input className="inputBox" type="password" name="newPassword" value={values.newPassword} onChange={updateValue} onBlur={updateIsFirst}></input>
+          <input
+            className="inputBox"
+            type="password"
+            name="newPassword"
+            value={values.newPassword}
+            onChange={updateValue}
+            onBlur={updateIsFirst}
+          ></input>
           <div className="errorMessage">{errors.newPassword}</div>
         </div>
         {/* 신 비밀번호 확인 */}
         <div className="changePassword__passwordConfirm changePasswordForm__component">
           <label>새 비밀번호 확인</label>
           <br />
-          <input className="inputBox" type="password" name="newPasswordConfirm" value={values.newPasswordConfirm} onChange={updateValue} onBlur={updateIsFirst} onFocus={lateValidateValue}></input>
+          <input
+            className="inputBox"
+            type="password"
+            name="newPasswordConfirm"
+            value={values.newPasswordConfirm}
+            onChange={updateValue}
+            onBlur={updateIsFirst}
+            onFocus={lateValidateValue}
+          ></input>
           <div className="errorMessage">{errors.newPasswordConfirm}</div>
         </div>
 
         <button
           type="sumbit"
           onClick={onChangePassword}
-          className={isValidated ? "registerForm__button register__button": "disabled registerForm__button register__button"}>
-            비밀번호 변경
+          className={
+            isValidated
+              ? 'registerForm__button register__button'
+              : 'disabled registerForm__button register__button'
+          }
+        >
+          비밀번호 변경
         </button>
-
       </div>
     </div>
-  )
+  );
 }
 
 export default ChangePassword;
--- frontend/src/router/user/common/FindPassword.js
+++ frontend/src/router/user/common/FindPassword.js
@@ -1,8 +1,8 @@
-import React, { useState, useContext } from 'react'
-import './FindPassword.css'
-import WriteEmail from 'components/user/WriteEmail'
-import SendPassword from 'components/user/SendPassword'
-import UserApi from 'api/UserApi'
+import React, { useState, useContext } from 'react';
+import './FindPassword.css';
+import WriteEmail from 'components/user/WriteEmail';
+import SendPassword from 'components/user/SendPassword';
+import UserApi from 'api/UserApi';
 import { store } from 'store/store.js';
 
 function FindPassword() {
@@ -10,9 +10,9 @@ function FindPassword() {
   const globalState = useContext(store);
   const { dispatch } = globalState;
   const { userKind } = globalState.value;
-  
-  const [ email, setEmail ] = useState("")
-  const [ isSend, setIsSend ] = useState(false)
+
+  const [email, setEmail] = useState('');
+  const [isSend, setIsSend] = useState(false);
 
   const onChange = (event) => {
     const {
@@ -20,19 +20,16 @@ function FindPassword() {
     } = event;
     if (name === 'userKind') {
       // userKind 전역 상태 관리 (store)
-      dispatch({ type: "SELECT_USER_KIND", value })
+      dispatch({ type: 'SELECT_USER_KIND', value });
     }
   };
 
   if (isSend) {
     return (
       <div className="findpw">
-        <img src="/images/logo.png" alt="logo"></img>
-        <SendPassword
-          email={email}
-        />
+        <SendPassword email={email} />
       </div>
-    )
+    );
   } else {
     return (
       <div className="findpw">
@@ -54,39 +51,40 @@ function FindPassword() {
             <span>비즈니스</span>
           </label>
         </div>
+        <u id="findPasswordTitle">비밀번호 찾기</u>
         <WriteEmail
-          setEmail={e => setEmail(e)}
+          setEmail={(e) => setEmail(e)}
           setIsSend={() => {
-            const data = {email: email}
-            setIsSend(true)
+            const data = { email: email };
+            setIsSend(true);
             if (userKind === 'player') {
               UserApi.findPwd(
                 data,
                 () => {
-                  alert("메일이 전송되었습니다!")
+                  alert('메일이 전송되었습니다!');
                 },
                 () => {
-                  setIsSend(false)
-                  alert("이메일을 확인해주세요!")
+                  setIsSend(false);
+                  alert('이메일을 확인해주세요!');
                 }
-              )
+              );
             } else if (userKind === 'business') {
               UserApi.findBusPwd(
                 data,
                 () => {
-                  alert("메일이 전송되었습니다!")
+                  alert('메일이 전송되었습니다!');
                 },
                 () => {
-                  setIsSend(false)
-                  alert("이메일을 확인해주세요!")
+                  setIsSend(false);
+                  alert('이메일을 확인해주세요!');
                 }
-              )
+              );
             }
           }}
         />
       </div>
-    )
+    );
   }
 }
 
-export default FindPassword
+export default FindPassword;
