532b76948e4319a48d05f141e28ab248336ff50e
jiyeongYun <yoonjy1106@naver.com>
2021-08-18 16:05:45 +0900

    Update : frontend\intro - frontend\intro : intro 화면 구현 Author : YJY Date : 21.08.18 16:05

---
+++ frontend/src/components/intro/Intro.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Intro.css";
+
+function Intro() {
+  return (
+    <div className="intro">
+      <img src="/images/intro.PNG" alt="intro" />
+    </div>
+  );
+}
+
+export default Intro;
