a7f7a5533084cdd9b75f158764c8ea1268d1c5ec
jiyeongYun <yoonjy1106@naver.com>
2021-08-19 11:20:32 +0900

    Update : frontend\findpassword - frontend\findpassword : 비밀번호 찾기페이지 css변경 Author : YJY Date : 21.08.19 11:20

--- backend/src/main/java/com/osds/bitz/service/GameService.java
+++ backend/src/main/java/com/osds/bitz/service/GameService.java
@@ -128,7 +128,7 @@ public class GameService {
      */
     public Game updateGame(GameRequest gameRequest) {
         Game updateGame = gameRepository.getGameById(gameRequest.getGameId());
-        Gym gym = updateGame.getGym();
+        Gym gym = updateGame.getGym();d
 
         updateGame.builder()
                 .id(gameRequest.getGameId())
