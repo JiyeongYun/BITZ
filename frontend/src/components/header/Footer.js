19c0bb653af660e9b0259f3dcff0b46d4801a709
권오우 <kwonow1@naver.com>
2021-08-23 08:49:21 +0900

    Update : footer - footer 연결 삭제 Author : KOW Date : 21.08.23 08:49

--- frontend/src/components/header/Footer.js
+++ frontend/src/components/header/Footer.js
@@ -7,21 +7,6 @@ const Footer = () => {
     e.preventDefault()
     window.open('https://youtu.be/xx9qydfb-xw')
   }
-
-  const toJira = (e) => {
-    e.preventDefault()
-    window.open('https://jira.ssafy.com/projects/S05P13A504/issues/S05P13A504-304?filter=allopenissues')
-  }
-
-  const toGit = (e) => {
-    e.preventDefault()
-    window.open('https://lab.ssafy.com/s05-webmobile2-sub3/S05P13A504')
-  }
-
-  const downloadPort = (e) => {
-    e.preventDefault()
-    window.open('https://lab.ssafy.com/s05-webmobile2-sub3/S05P13A504/-/raw/develop/exec/OSDS_BITZ_%EC%B5%9C%EC%A2%85%EB%B0%9C%ED%91%9C.pdf?inline=false')
-  }
   return (
     <footer>
       <section className="ft-main">
@@ -29,7 +14,6 @@ const Footer = () => {
           <h2 className="ft-title">About</h2>
           <ul>
             <li><Link to="/intro">Intro</Link></li>
-            <li><Link to="#" onClick={downloadPort}>Portfolio</Link></li>
             <li><Link to="#" onClick={toYoutube}>Demo</Link></li>
           </ul>
         </div>
@@ -37,8 +21,6 @@ const Footer = () => {
 
       <section className="ft-social">
         <ul className="ft-social-list">
-          <li><Link to="#" onClick={toGit}><i className="fab fa-gitlab"></i></Link></li>
-          <li><Link to="#" onClick={toJira}><i className="fab fa-jira"></i></Link></li>
           <li><Link to="#" onClick={toYoutube}><i className="fab fa-youtube"></i></Link></li>
         </ul>
       </section>
